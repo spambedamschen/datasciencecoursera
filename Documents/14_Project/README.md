77ba5963866508f7f5f1de29bc64e16249087c20
spambedamschen <spambedamschen@gmx.de>
2015-01-26 01:17:51 +0100

    update README

---
+++ Documents/14_Project/README.md
@@ -0,0 +1,12 @@
+## Step 1
+Merging Data with cbind for x_train, subject_train and y_train and the equivalent of the test data.
+After that I rowbinded the two data frames - the one for train and the other one for test
+
+## Step 2
+By extracting the second column of the feature file I got the names
+with the names(data) comand I added the names to the variable
+
+## Step 3
+THe third step for me was the subsetting of the columns including std and mean.
+Unfortunately somehow I did not get the hang of using the grep function and failed at this point :(
+
--- Documents/14_Project/run_analysis.R
+++ Documents/14_Project/run_analysis.R
@@ -1,1 +1,26 @@
-  fileUrl <- ""
+
+    x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+    subject_train  <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+    y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+    
+    train <- x_train
+    train <- cbind(train, subject_train)
+    train <- cbind(train, y_train)
+    
+    x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+    subject_test  <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+    y_test <- read.table("./UCI HAR Dataset/test/y_test.txt") 
+    
+    test <- x_test
+    test <- cbind(test, subject_test)
+    test <- cbind(test, y_test)
+    
+    data <- train
+    data <- rbind(data, test)
+    
+    features <- read.table("./UCI HAR Dataset/features.txt")
+    ListOfNames <- as.character(features[,2]) 
+    
+    names(data)<-c(ListOfNames,"Subjectname","yname")
+    
+    Namen <- grep("Mean, std", features, ignore.case = T)
