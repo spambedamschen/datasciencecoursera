63ea71e0e6897c126fb45d128eef73680219f03d
spambedamschen <spambedamschen@gmx.de>
2015-02-20 03:13:42 +0100

    new course

--- Documents/14_Project/run_analysis.R
+++ Documents/14_Project/run_analysis.R
@@ -1,26 +1,29 @@
-
+    train <- data.frame()
     x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
     subject_train  <- read.table("./UCI HAR Dataset/train/subject_train.txt")
     y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
     
-    train <- x_train
-    train <- cbind(train, subject_train)
+    train <- subject_train
     train <- cbind(train, y_train)
+    train <- cbind(train, x_train)
     
     x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
     subject_test  <- read.table("./UCI HAR Dataset/test/subject_test.txt")
     y_test <- read.table("./UCI HAR Dataset/test/y_test.txt") 
     
-    test <- x_test
-    test <- cbind(test, subject_test)
+    test <- subject_test
     test <- cbind(test, y_test)
+    test <- cbind(test, x_test)
     
-    data <- train
-    data <- rbind(data, test)
+    data <- test
+    data <- rbind(data, train)
     
     features <- read.table("./UCI HAR Dataset/features.txt")
     ListOfNames <- as.character(features[,2]) 
     
-    names(data)<-c(ListOfNames,"Subjectname","yname")
+    names(data)<-c("Subjectname","yname", ListOfNames)
     
-    Namen <- grep("Mean, std", features, ignore.case = T)
+    stddata<-data[,grep('std',names(data))]
+    meandata<-data[,grep('mean',names(data))]
+    combineddata <- cbind(data$Subjectname, data$yname, stddata, meandata)
+    
